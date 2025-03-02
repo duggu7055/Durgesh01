ce9d83faae0d68cf415fdc92f1482a4e56cc2010
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-03-02 23:28:55 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "s3" {
-    bucket         = "demo-bucket-abcdefd12345"
-    key            = "terraform/state"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks" # Optional: Lock table for state locking
-    encrypt        = true              # Optional: Encrypt the state file
-  }
-}
 
 # Define the VPC
 resource "aws_vpc" "main_vpc" {
