38ebf7fedd15e8f9acd9168307d6face44ce35df
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-03-02 23:28:42 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,9 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.88.0"
     }
+  }
+  
+  backend "s3" {
+    bucket         = "demo-bucket-abcdefd12345"
+    key            = "terraform/state"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks" # Optional: Lock table for state locking
+    encrypt        = true              # Optional: Encrypt the state file
   }
 }
 
@@ -11,5 +19,5 @@ provider "aws" {
   # Configuration options
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "ap-south-1"
+  region     = "ap-south-1"
 }
