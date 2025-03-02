f08bd7d4db3c7326c9db1afacd2cc569738c8041
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-03-02 23:17:01 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.88.0"
+      version = "~> 3.0"
     }
   }
+}
 
   backend "s3" {
     bucket         = "demo-bucket-abcdefd12345"      
